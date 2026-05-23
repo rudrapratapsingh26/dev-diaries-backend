9817363193339234dd330322db3f6119c39f71f0
Rudra Pratap Singh <rudrapratapsingh.fzd@gmail.com>
2026-05-24 01:43:20 +0530

    chore: add express app and server entry point

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// app.js
+import express from "express";
+const app = express();
+
+// middleware, routes, etc.
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import app from "./app.js";
+import connectDB from "./database/database.js";
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Server failed to start", error);
+  }
+};
+
+startServer();
